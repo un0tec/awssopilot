36da3fa9e0210297402e51084e83fb07bb949eca
un0tec <55037574+un0tec@users.noreply.github.com>
2024-12-02 15:45:34 +0100

    feat: first version

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # awssopilot
+
+[![License: MIT](https://img.shields.io/github/license/username/awssopilot?color=orange&cache=none)](LICENSE)
+[![Release](https://img.shields.io/github/v/release/username/awssopilot?color=green&label=Release)](https://github.com/username/awssopilot/releases/latest)
+
+1. :notebook_with_decorative_cover: [Description](#-description)
+2. :warning: [Before running](#-before-running)
+3. :writing_hand: [Syntax](#-syntax)
+4. :hammer: [Usage](#-usage)
+5. :bookmark_tabs: [Options](#-options)
+6. :monocle_face: [Examples](#-examples)
+7. :page_with_curl: [License](#-license)
+8. :heart: [Contribution](#-contributing)
+
+## # Description
+
+`awssopilot` is a command-line tool that automates AWS SSO login for multiple profiles. It supports various login methods such as email/password, phone call, or app-based verification. Additionally, it integrates with [yawsso](https://github.com/victorskl/yawsso) to configure IAM roles once the login process is completed.
+
+## # Before Running
+
+Ensure that your AWS SSO profiles are configured using `aws sso configure` and your profiles are configured in a JSON file named `awssopilot.config`. The format of the configuration file should include the following details:
+
+- `profiles`: An array of AWS SSO profiles to log into.
+- `email`: The email address associated with the AWS account.
+- `password`: The password for the AWS account.
+- `phone`: Last two digits of the phone number for phone call-based authentication.
+- `type`: The authentication type (`call` for phone call or `app` for app-based verification).
+
+The `awssopilot.config` file should be placed in the user's home directory (Linux `~/awssopilot.config`, Windows `%userprofile%/awssopilot.config`).
+
+## # Syntax
+
+The general syntax for using `awssopilot` is:
+
+    awssopilot [OPTIONS]
+
+## # Usage
+
+Basic usage example:
+
+    awssopilot
+
+This will automatically attempt to log into all profiles specified in the `awssopilot.config` file.
+
+## # Options
+
+N/A
+
+## # Examples
+
+N/A
+
+## # License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+## # Contributing
+
+We welcome contributions to improve the functionality of `awssopilot`! Here's how you can contribute:
+
+1. Fork the project
+2. Create a new feature branch (`git checkout -b feature/YourFeature`)
+3. Make your changes and commit (`git commit -m 'Add feature'`)
+4. Push to your branch (`git push origin feature/YourFeature`)
+5. Open a pull request
+
+Your contributions will be greatly appreciated!
+
+:star: Feel free to contribute :star:
---
+++ awssopilot.js
@@ -0,0 +1,130 @@
+import puppeteer from 'puppeteer';
+import { readFileSync } from 'fs';
+import { setTimeout } from 'timers/promises';
+import { execa } from 'execa';
+import os from 'os'
+import pkg from './package.json' with {type: 'json'};
+import { checkUpdate } from './update-notifier.js';
+
+const updateAvailable = await checkUpdate({
+    author: pkg.author,
+    repository: pkg.repository.name,
+    name: pkg.name,
+    version: pkg.version
+})
+
+if (updateAvailable) { process.exit(); }
+
+init().catch((error) => {
+    console.error(error.message);
+    process.exit(1);
+});
+
+async function init() {
+
+    // config
+    const data = readFileSync(os.homedir() + "/awssopilot.config", "utf8");
+    const config = JSON.parse(data);
+
+    // check aws installed
+    await execa`aws --version`;
+
+    // check yawsso installed
+    await execa`yawsso --version`;
+
+    for (const profile of config.profiles) {
+
+        console.log(`Setting profile: ${profile}`);
+
+        const execaProcess = execa({ reject: false })`aws sso login --profile ${profile} --no-browser`;
+
+        for await (const line of execaProcess) {
+            if (!line.includes('?user_code')) {
+                continue;
+            }
+            const url = line;
+
+            // browser
+            const browser = await puppeteer.launch({ headless: true, args: ['--lang=en'] });
+            const page = (await browser.pages())[0];
+
+            // load page
+            console.log(`    Loading url: ${url}`);
+            await page.goto(url);
+
+            // confirm code
+            console.log('    Approving code...');
+            await page.waitForSelector('#cli_verification_btn');
+            await page.click('#cli_verification_btn');
+
+            // user
+            console.log('    Logging user...');
+            await page.waitForSelector('input[type="email"]');
+            await page.type('input[type="email"]', config.email);
+            await page.click('input[type="submit"]');
+
+            // password
+            await page.waitForSelector('input[type="password"]');
+            await setTimeout(1000);
+            await page.type('input[type="password"]', config.password);
+            await page.click('input[type="submit"]');
+
+            // call
+            if (config.type === 'call') {
+                // login with call
+                console.log('    Logging with phone call...');
+                await page.waitForSelector('#signInAnotherWay');
+                await page.click('#signInAnotherWay');
+
+                // select phone
+                await page.waitForSelector('#idDiv_SAOTCS_Title');
+                await page.locator(`div ::-p-text("Call +XX XXXXXXX${config.phone}")`).click();
+                console.log('    Awaiting approval call...');
+            }
+
+            // app
+            if (config.type === 'app') {
+                // show code
+                console.log('    Loading app code...');
+                const element = await page.waitForSelector('#idRichContext_DisplaySign');
+                const text = await page.evaluate(element => element.textContent, element);
+                console.log(`    Awaiting approval of code: ${text}`);
+            }
+
+            // submit stay logged
+            await page.waitForSelector('#KmsiDescription', { delay: 60 });
+            await page.click('input[type="submit"]');
+
+            // allow cookies
+            await page.waitForSelector('button[aria-label="Accept all cookies"]');
+            await page.click('button[aria-label="Accept all cookies"]');
+
+            // allow access
+            console.log('    Approving access...');
+            await page.locator(`span ::-p-text("Allow access")`).wait();
+            await page.locator(`span ::-p-text("Allow access")`).click();
+            await page.locator(`div ::-p-text("Request approved")`).wait();
+
+            // close browser
+            await browser.close();
+
+            // close process
+            console.log('    Awaiting graceful time...');
+            await setTimeout(3000);
+            execaProcess.kill();
+
+            // yawsso
+            console.log('    Executing YAWSSO...');
+            await execa`yawsso -p ${profile}:${profile}-cred`;
+
+            // logs
+            const logMessage = `    IAM profile '${profile}-cred' configured `;
+            console.log('-'.repeat(logMessage.length));
+            console.log(`    SSO profile '${profile}' token renewed`);
+            console.log(logMessage);
+            console.log('-'.repeat(logMessage.length));
+            break;
+        }
+    }
+
+}
